c42ea417fc4355ba2b58603e11e50e3fd085e0bb
Deniz Ata ES <denizataes@hotmail.com>
2022-11-26 13:30:41 +0300

    Initial Commit

---
+++ Seans/SeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeansApp.swift
+//  Seans
+//
+//  Created by Deniz Ata Eş on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
